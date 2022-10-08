6ecc8d8b5c36fb47461456277672038516210fa0
Evgeniy Kostin <xqvynfs6f8@privaterelay.appleid.com>
2022-10-08 13:07:57 +0300

    Initial Commit

---
+++ Creating a form/Creating_a_formApp.swift
@@ -0,0 +1,17 @@
+//
+//  Creating_a_formApp.swift
+//  Creating a form
+//
+//  Created by Evgeniy Kostin on 08.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Creating_a_formApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
